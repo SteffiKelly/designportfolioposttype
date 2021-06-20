fd756b6833520cea958c27a0f2d7495d36847c53
Steffi Kelly <info@steffikelly.com>
2021-06-20 23:07:37 +0100

    Update for use on live site
    Changed text domain and made terms more specific to avoid conflicts

--- design-project.php
+++ design-project.php
@@ -1,27 +1,27 @@
 <?php
 /**
 * Plugin Name:   Design Project Post Type
-* Plugin URI:    https://steffikelly.com
-* Description:   Adds a widget for a call to action box
+* Plugin URI:    https://github.com/SteffiKelly/designportfolioposttype
+* Description:   Adds design portfolio custom post type
 * Version:       1.0
 * Author:        Steffi Kelly
 * Author URI:    https://steffikelly.com
-* Text Domain: wpmu
+* Text Domain: sk21
 * Domain Path: /languages
 */
 
-function wpmu_create_post_type() {
+function sk21_create_design_post_type() {
 	$labels = array(
-		'name' => __( 'Design Projects', 'wpmu' ),
-		'singular_name' => __( 'Design Project', 'wpmu' ),
-		'add_new' => __( 'New Design Project', 'wpmu' ),
-		'add_new_item' => __( 'Add New Design Project', 'wpmu' ),
-		'edit_item' => __( 'Edit Design Project', 'wpmu' ),
-		'new_item' => __( 'New Design Project', 'wpmu' ),
-		'view_item' => __( 'View Design Project', 'wpmu' ),
-		'search_items' => __( 'Search Design Projects', 'wpmu' ),
-		'not_found' =>  __( 'No Design Projects Found', 'wpmu' ),
-		'not_found_in_trash' => __( 'No Design Projects found in Trash', 'wpmu' ),
+		'name' => __( 'Design Projects', 'sk21' ),
+		'singular_name' => __( 'Design Project', 'sk21' ),
+		'add_new' => __( 'New Design Project', 'sk21' ),
+		'add_new_item' => __( 'Add New Design Project', 'sk21' ),
+		'edit_item' => __( 'Edit Design Project', 'sk21' ),
+		'new_item' => __( 'New Design Project', 'sk21' ),
+		'view_item' => __( 'View Design Project', 'sk21' ),
+		'search_items' => __( 'Search Design Projects', 'sk21' ),
+		'not_found' =>  __( 'No Design Projects Found', 'sk21' ),
+		'not_found_in_trash' => __( 'No Design Projects found in Trash', 'sk21' ),
 	);
 	$args = array(
 		'labels' => $labels,
@@ -41,20 +41,20 @@ function wpmu_create_post_type() {
 	);
 	register_post_type( 'design-project', $args );
 }
-add_action( 'init', 'wpmu_create_post_type' );
+add_action( 'init', 'sk21_create_design_post_type' );
 
-function wpmu_register_taxonomy() {
+function sk21_register_service_taxonomy() {
 
   $labels = array(
-		'name'              => __( 'Services', 'wpmu' ),
-		'singular_name'     => __( 'Service', 'wpmu' ),
-		'search_items'      => __( 'Search Services', 'wpmu' ),
-		'all_items'         => __( 'All Services', 'wpmu' ),
-		'edit_item'         => __( 'Edit Services', 'wpmu' ),
-		'update_item'       => __( 'Update Services', 'wpmu' ),
-		'add_new_item'      => __( 'Add New Services', 'wpmu' ),
-		'new_item_name'     => __( 'New Service Name', 'wpmu' ),
-		'menu_name'         => __( 'Services', 'wpmu' ),
+		'name'              => __( 'Services', 'sk21' ),
+		'singular_name'     => __( 'Service', 'sk21' ),
+		'search_items'      => __( 'Search Services', 'sk21' ),
+		'all_items'         => __( 'All Services', 'sk21' ),
+		'edit_item'         => __( 'Edit Services', 'sk21' ),
+		'update_item'       => __( 'Update Services', 'sk21' ),
+		'add_new_item'      => __( 'Add New Services', 'sk21' ),
+		'new_item_name'     => __( 'New Service Name', 'sk21' ),
+		'menu_name'         => __( 'Services', 'sk21' ),
 	);
 
 	$args = array(
@@ -69,21 +69,21 @@ function wpmu_register_taxonomy() {
 	register_taxonomy( 'service', array( 'post', 'design-project' ), $args);
 
 }
-add_action( 'init', 'wpmu_register_taxonomy' );
+add_action( 'init', 'sk21_register_service_taxonomy' );
 
-function wpmu_add_categories_to_pages() {
+function sk21_add_categories_to_design_pages() {
 	register_taxonomy_for_object_type( 'category', 'page' );
 }
-add_action( 'init', 'wpmu_add_categories_to_pages' );
+add_action( 'init', 'sk21_add_categories_to_design_pages' );
 
 /**********************************************************************************
-wpmu_design_project_i18n - registers text domain for i18n
+sk21_design_project_i18n - registers text domain for i18n
 **********************************************************************************/
-function wpmu_design_project_i18n() {
+function sk21_design_project_i18n() {
 
-	load_plugin_textdomain( 'wpmu', false, plugin_basename( dirname( __FILE__ ) ) . '/languages' );
+	load_plugin_textdomain( 'sk21', false, plugin_basename( dirname( __FILE__ ) ) . '/languages' );
 
 }
-add_action( 'plugins_loaded', 'wpmu_design_project_i18n' );
+add_action( 'plugins_loaded', 'sk21_design_project_i18n' );
 
 ?>
